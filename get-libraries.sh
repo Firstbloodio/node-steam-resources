3b222091ca0018b46adc5f6086859f0d7dca9a22
Armando Soriano <asoriano.dev@gmail.com>
2022-02-18 02:08:58 +0100

    chore: improving get-libraries script to remove .gitattributes and .gitignore from protobufs

--- get-libraries.sh
+++ get-libraries.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf protobufs protobuf_gen steam_language
-git clone https://github.com/SteamDatabase/Protobufs protobufs && rm -rf protobufs/.git \
+git clone https://github.com/SteamDatabase/Protobufs protobufs && rm -rf protobufs/.git && rm .git* \
   && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/ProtobufGen -d protobuf_gen \
   && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/SteamLanguage -d steam_language
