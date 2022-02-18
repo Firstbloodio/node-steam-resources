a26a4d261f12407bdeb4ea65ce2bbc1f34436262
Armando Soriano <asoriano.dev@gmail.com>
2022-02-18 02:05:57 +0100

    chore: profobufs should not be a submodule

--- get-libraries.sh
+++ get-libraries.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -rf protobufs protobuf_gen steam_language
-git clone https://github.com/SteamDatabase/Protobufs protobufs && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/ProtobufGen -d protobuf_gen && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/SteamLanguage -d steam_language
+git clone https://github.com/SteamDatabase/Protobufs protobufs && rm -rf protobufs/.git \
+  && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/ProtobufGen -d protobuf_gen \
+  && ./git-dir.sh -r https://github.com/SteamRE/SteamKit -s Resources/SteamLanguage -d steam_language
